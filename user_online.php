6759103b7b5fa10c990f6db54a54147559c20f93
Sergij Osadchy <sergijlys@gmail.com>
2014-02-12 03:27:45 +0200

    For synch

--- user_online.php
+++ user_online.php
@@ -1,20 +1,21 @@
 <?php
 
-    $link = mysql_connect("localhost", "root", "");
+    $link = mysql_connect("localhost", "root", ""); // Конект
     mysql_select_db("test");
 
-	$ip = mysql_real_escape_string($_SERVER['REMOTE_ADDR']);
+	$ip = mysql_real_escape_string($_SERVER['REMOTE_ADDR']); // IP юзера
 	
     $query = "INSERT INTO `user_online` VALUES (null, '".$ip."', '".time()."')
-				ON DUPLICATE KEY UPDATE online_at='".time()."'";
+				ON DUPLICATE KEY UPDATE online_at='".time()."'"; // Вставляємо або апдейтимо, якщо вже є.
 	$result = mysql_query($query);
 	
-			$query = "SELECT count(`id`) as `cnt` FROM `user_online` WHERE (`online_at` + 5)>'".time()."' ";
+	$query = "SELECT count(`id`) as `cnt` FROM `user_online` WHERE (`online_at` + 5)>'".time()."' "; // Кількість онлайн
+	
     $result = mysql_query($query);
 
     $array = mysql_fetch_array($result, MYSQL_ASSOC);
 	
-	echo $array['cnt'];
+	echo $array['cnt']; // Виводимо
 
     mysql_close($link);
 ?>
